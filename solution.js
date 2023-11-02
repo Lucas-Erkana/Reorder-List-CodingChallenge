27392f8f6408dcd60ebb67b28a3c698839e25189
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-02 11:25:40 +0200

    Solution 2 November 2023

--- solution.js
+++ solution.js
@@ -1,9 +1,57 @@
-var functionname = function(parameter) {
-//Write code here
-  };
+var reorderList = function(head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let slow = head;
+  let fast = head;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null;
+  let curr = slow.next;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  slow.next = null;
+
+  let p1 = head;
+  let p2 = prev;
+  while (p1 && p2) {
+    const next1 = p1.next;
+    const next2 = p2.next;
+    p1.next = p2;
+    p2.next = next1;
+    p1 = next1;
+    p2 = next2;
+  }
+
+  printLinkedList(head);
+};
+
+// Helper function to print the values of the linked list
+function printLinkedList(head) {
+  let node = head;
+  const values = [];
+  while (node) {
+    values.push(node.val);
+    node = node.next;
+  }
+  console.log(values);
+}
+
+// Example 1
+const head1 = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: null } } } };
+reorderList(head1); // [1, 4, 2, 3]
+
+// Example 2
+const head2 = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: { val: 5, next: null } } } } };
+reorderList(head2); // [1, 5, 2, 4, 3]
 
 
-  let parameter = 'test data'
-console.log(functionname(parameter)) //expected result true
-
-module.exports = functionname;
+module.exports = reorderList;
--- tests.js
+++ tests.js
@@ -1,6 +1,45 @@
-const funtionname = require('./solution');
+const reorderList = require('./solution');
 const assert = require('assert');
 
 describe('Tests', function () {
-//Write tests here
+  it('should reorder the list [1, 2, 3, 4] to [1, 4, 2, 3]', function () {
+    const head = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: null } } } };
+    reorderList(head);
+    assert.deepStrictEqual(getLinkedListValues(head), [1, 4, 2, 3]);
+  });
+
+  it('should reorder the list [1, 2, 3, 4, 5] to [1, 5, 2, 4, 3]', function () {
+    const head = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: { val: 5, next: null } } } } };
+    reorderList(head);
+    assert.deepStrictEqual(getLinkedListValues(head), [1, 5, 2, 4, 3]);
+  });
+
+  it('should reorder the list [1] to [1]', function () {
+    const head = { val: 1, next: null };
+    reorderList(head);
+    assert.deepStrictEqual(getLinkedListValues(head), [1]);
+  });
+
+  it('should reorder the list [1, 2] to [1, 2]', function () {
+    const head = { val: 1, next: { val: 2, next: null } };
+    reorderList(head);
+    assert.deepStrictEqual(getLinkedListValues(head), [1, 2]);
+  });
+
+  it('should reorder the list [1, 2, 3, 4, 5, 6] to [1, 6, 2, 5, 3, 4]', function () {
+    const head = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: { val: 5, next: { val: 6, next: null } } } } } };
+    reorderList(head);
+    assert.deepStrictEqual(getLinkedListValues(head), [1, 6, 2, 5, 3, 4]);
+  });
+
+  // Helper function to get the values of the linked list
+  function getLinkedListValues(head) {
+    let node = head;
+    const values = [];
+    while (node) {
+      values.push(node.val);
+      node = node.next;
+    }
+    return values;
+  }
 });
